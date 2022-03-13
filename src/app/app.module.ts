ac99a293263b44ce1560d715665934d414224f81
Sebastien Matton <wolfsepi@gmail.com>
2022-03-13 21:02:29 +0100

    added pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
+import { FormsModule } from '@angular/forms';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, CourseComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, CoursesComponent, CourseComponent, SummaryPipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [CoursesService],
   bootstrap: [AppComponent],
 })
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,19 +4,55 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'courses',
   template: `
-    <h2>{{ title }}</h2>
+    <!-- <h2>{{ title }}</h2>
     <ul>
       <li *ngFor="let course of courses">
         {{ course }}
       </li>
     </ul>
+    <i class="bi-github" role="img" aria-label="GitHub"></i>
+    <img src="{{ imageUrl }}" />
+    <table>
+      <tr>
+        <td [attr.colSpan]="colSpan"></td>
+      </tr>
+    </table>
+    <button class="btn btn-primary" [class.avtive]="isActive">Save</button
+    ><br />
+    <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button
+    ><br />
+    <div (click)="onClickDiv()">
+      <button (click)="onSave($event)">Save</button>
+    </div> -->
+    <!-- <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" /> -->
+    {{ text | summary: 10 }}
   `,
 })
 export class CoursesComponent {
   title = 'List of courses';
   courses: string[];
+  imageUrl = 'https://loremflickr.com/400/260';
+  colSpan: number;
+  isActive = true;
+  email = 'me@example.com';
+  text =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua';
 
   constructor(service: CoursesService) {
     this.courses = service.getCrouses();
+    this.colSpan = 2;
+  }
+
+  onSave($event) {
+    $event.stopPropagation();
+    console.log('Button click', $event);
+  }
+
+  onClickDiv() {
+    console.log('Div click');
+  }
+
+  onKeyUp() {
+    console.log(this.email);
   }
 }
