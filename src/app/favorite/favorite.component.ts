407841f33af7bcf35c05d9fa33d1cf35b57a178c
Sebastien Matton <Sebastien.Matton@macq.eu>
2022-04-18 17:45:35 +0200

    added output alias

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -3,24 +3,22 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.scss']
+  styleUrls: ['./favorite.component.scss'],
 })
 export class FavoriteComponent implements OnInit {
-  @Input("isFavorite") isFavorite!: boolean;
-  @Output() change = new EventEmitter();
+  @Input('isFavorite') isFavorite!: boolean;
+  @Output('change') click = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onClick() {
     this.isFavorite = !this.isFavorite;
-    this.change.emit({ newValue: this.isFavorite });
+    this.click.emit({ newValue: this.isFavorite });
   }
-
 }
 
 export interface FavoriteChangedEventsArgs {
-  newValue: boolean
+  newValue: boolean;
 }
